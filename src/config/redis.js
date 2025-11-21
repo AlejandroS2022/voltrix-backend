ea565edb53efba8cf0f237f00ac1cccc67bb4352
AlejandroSuazo2022 <alejandro.suazo@webstudioswest.com>
2025-11-21 18:22:23 -0400

    Returning a Redis config no longer needed.

--- src/config/redis.js
+++ src/config/redis.js
@@ -4,14 +4,6 @@ const REDIS_URL = process.env.REDIS_URL;
 
 let redis = new Redis(REDIS_URL);
 
-if (process.env.NODE_ENV === 'production') {
-  redis = new Redis(REDIS_URL, {
-    tls: {
-      rejectUnauthorized: false
-    }
-  });
-}
-
 redis.on("connect", () => console.log("Redis connected"));
 redis.on("error", (err) => console.error("Redis error: ", err));
 
