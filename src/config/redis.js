9944adac99b954f14c26eea240b05b71054fdac6
AlejandroSuazo2022 <alejandro.suazo@webstudioswest.com>
2025-11-21 17:46:39 -0400

    Testing Redis config.

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,6 +1,16 @@
 const Redis = require("ioredis");
 
-const redis = new Redis(process.env.REDIS_URL);
+const REDIS_URL = process.env.REDIS_URL;
+
+let redis = new Redis(REDIS_URL);
+
+if (process.env.NODE_ENV === 'production') {
+  redis = new Redis(REDIS_URL, {
+    tls: {
+      rejectUnauthorized: false
+    }
+  });
+}
 
 redis.on("connect", () => console.log("Redis connected"));
 redis.on("error", (err) => console.error("Redis error: ", err));
